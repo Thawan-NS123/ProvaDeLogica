1fa42a72d288017139819f77ae9900b9dba824db
Thawan Nasc <thawann.silva34@gmail.com>
2022-10-14 09:43:59 -0300

    Prova 14/10 concluida

--- site/src/functions/AcaiFunction/Acai.js
+++ site/src/functions/AcaiFunction/Acai.js
@@ -3,6 +3,7 @@ export async function calcularTotalFunction(qtdPequena, qtdMedia, qtdGrande, vlD
     if(qtdPequena < 0 || qtdMedia < 0 || qtdGrande < 0 || vlDesc < 0 ){
       return 0
     }
+    
 
     let total = (await qtdGrande * 17.50 + qtdMedia * 15.00 + qtdPequena * 13.50) * (vlDesc / 100 - 1)   
 
